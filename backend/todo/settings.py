17af3a7b25e7930b78b6bbec45d3a82c76bd181f
robert <zeonita@gmail.com>
2025-02-17 08:13:48 +0200

    Replace hardcoded SECRET_KEY with environment variable for improved security

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ya#o8g2sw7p=&#b++wabhfm_8%)c^99lgx!_2q+ya!fzd@)uz_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
